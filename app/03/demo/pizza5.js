185eab4073b765d067cf90f3e7bec47f88104bdb
Sonia Pini <sonia.pini@nispro.it>
2015-04-17 14:33:35 +0200

    eliminato controller Order da pizza5.js demo

--- app/03/demo/pizza5.js
+++ app/03/demo/pizza5.js
@@ -28,20 +28,5 @@ function PizzaMenuCtrl($scope) {
         ];
 }
 
-function OrderCtrl($scope) {
-    $scope.order = {
-        pizzaCount : 0,
-        items : [],
-        totalPrice : 0
-    };
-
-    $scope.add = function(pizza)
-    {
-        $scope.order.items.push(pizza);
-        $scope.order.pizzaCount = $scope.order.pizzaCount + 1;
-        $scope.order.totalPrice = $scope.order.totalPrice + pizza.price; //TODO REFACTOR
-    }
-}
-
 angular.module("pizza5App").controller("PizzaMenuCtrl",PizzaMenuCtrl);
-angular.module("pizza5App").controller("OrderCtrl",OrderCtrl);
+
